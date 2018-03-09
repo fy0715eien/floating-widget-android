2f276e65474bd82b6ac023f65b2fb38a992ec589
YUE FANG <fy0715eien@gmail.com>
2018-03-09 20:23:12 +0800

    SharedPreference now works normally

--- app/src/main/java/com/example/fy071/floatingwidget/MainActivity.java
+++ app/src/main/java/com/example/fy071/floatingwidget/MainActivity.java
@@ -9,7 +9,6 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
