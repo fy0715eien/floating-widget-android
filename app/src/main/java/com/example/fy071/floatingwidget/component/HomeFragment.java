2f3fc859cb05ed610ad4eddd125cfb95cbd9c6a5
YUE FANG <fy0715eien@gmail.com>
2018-03-15 18:08:04 +0800

    修正onBackPressed，修正权限获取 已知问题：切换Fragment时卡顿

--- app/src/main/java/com/example/fy071/floatingwidget/component/HomeFragment.java
+++ app/src/main/java/com/example/fy071/floatingwidget/component/HomeFragment.java
@@ -1,36 +1,31 @@
 package com.example.fy071.floatingwidget.component;
 
 
-import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fy071.floatingwidget.R;
-import com.example.fy071.floatingwidget.entity.Pet;
 
 
-public class HomeFragment extends Fragment{
-    private static final String TAG = "HomeFragment";
-    Pet pet;
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
     public HomeFragment() {
+        // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View currentView = inflater.inflate(R.layout.fragment_home, container, false);
-        pet = new Pet();
-        pet.addSelfToView(currentView);
-        return currentView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
 }
