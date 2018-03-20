cc9ff28ed6e248f0525e51b3a0a55db1ad9356cc
YUE FANG <fy0715eien@gmail.com>
2018-03-20 23:18:36 +0800

    改动：重构，将宠物属性装入实体类pet 待添加： 1.宠物动画； 2.宠物动作 3.模型选择，背景选择，语言选择 已知问题：尚未发现

--- app/src/main/java/com/example/fy071/floatingwidget/component/HomeFragment.java
+++ app/src/main/java/com/example/fy071/floatingwidget/component/HomeFragment.java
@@ -2,69 +2,27 @@ package com.example.fy071.floatingwidget.component;
 
 
 import android.app.Fragment;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 import com.example.fy071.floatingwidget.R;
-import com.example.fy071.floatingwidget.util.Key;
+import com.example.fy071.floatingwidget.entity.Pet;
 
 
-public class HomeFragment extends Fragment implements View.OnTouchListener {
+public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
-    float fingerStartX, fingerStartY, viewStartX, viewStartY;
-    FrameLayout pet;
-    ImageView imageView;
-    SharedPreferences sharedPreferences;
+    Pet pet;
     public HomeFragment() {
     }
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View currentView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
-
-        pet = currentView.findViewById(R.id.layout_pet);
-        imageView = pet.findViewById(R.id.imageView_pet);
-        pet.setOnTouchListener(this);
-        pet.setX(sharedPreferences.getFloat(Key.PET_LAST_X, 0));
-        pet.setY(sharedPreferences.getFloat(Key.PET_LAST_Y, 0));
+        pet = new Pet();
+        pet.addSelfToView(currentView);
         return currentView;
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                fingerStartX = event.getRawX();
-                fingerStartY = event.getRawY();
-                viewStartX = v.getX();
-                viewStartY = v.getY();
-                imageView.setImageResource(R.drawable.test2);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "onTouch: move");
-                v.setX(event.getRawX() + viewStartX - fingerStartX);
-                v.setY(event.getRawY() + viewStartY - fingerStartY);
-                break;
-            case MotionEvent.ACTION_UP:
-                imageView.setImageResource(R.drawable.test);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putFloat(Key.PET_LAST_X, v.getX());
-                editor.putFloat(Key.PET_LAST_Y, v.getY());
-                editor.apply();
-                break;
-        }
-        return true;
     }
 }
