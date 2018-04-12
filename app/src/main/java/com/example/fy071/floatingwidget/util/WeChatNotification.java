9e7d75391c9d31a634a817010e2a3fab88802381
YUE FANG <fy0715eien@gmail.com>
2018-04-12 23:14:59 +0800

    可能修复NotificationListenerService

--- app/src/main/java/com/example/fy071/floatingwidget/util/WeChatNotification.java
+++ app/src/main/java/com/example/fy071/floatingwidget/util/WeChatNotification.java
@@ -2,37 +2,64 @@ package com.example.fy071.floatingwidget.util;
 
 
 import android.app.Notification;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
-import android.widget.Toast;
+
+import com.example.fy071.floatingwidget.component.service.FloatingViewService;
 
 
 public class WeChatNotification extends NotificationListenerService {
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        ComponentName componentName = new ComponentName(this, FloatingViewService.class);
+        if (Build.VERSION.SDK_INT >= 24) {
+            requestRebind(componentName);
+        } else {
+            toggleNotificationListenerService(componentName);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void toggleNotificationListenerService(ComponentName componentName) {
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
     private static final String TAG = "WeChatNotification";
+
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        Log.d(TAG, "onNotificationPosted: "+getPackageName());
+        Log.d(TAG, "onNotificationPosted: " + getPackageName());
         if (!"com.tencent.mm".equals(sbn.getPackageName())) {
             return;
         } //不是微信的通知过滤掉
         Notification notification = sbn.getNotification();
-        Log.d(TAG, "onNotificationPosted: "+notification);
+        Log.d(TAG, "onNotificationPosted: " + notification);
         if (notification == null) {
             return;
         }
         Bundle extras = notification.extras;
-        Log.d(TAG, "onNotificationPosted: "+extras);
+        Log.d(TAG, "onNotificationPosted: " + extras);
 
         if (extras != null) {
             //获取标题
             String title = extras.getString(Notification.EXTRA_TITLE, "");
             // 获取通知内容
             String content = extras.getString(Notification.EXTRA_TEXT, "");
-            Log.d(TAG, "onNotificationPosted: "+title);
-            Log.d(TAG, "onNotificationPosted: "+content);
+            Log.d(TAG, "onNotificationPosted: " + title);
+            Log.d(TAG, "onNotificationPosted: " + content);
             System.out.print(content);
         }
     }
